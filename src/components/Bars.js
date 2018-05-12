7c64c5fe9534e4e073c88dae974be1c747392cbb
unknown <7560635+Everspace@users.noreply.github.com>
2018-05-12 01:22:24 -0700

    Reimplement Progress from reactstrap as ProgressBar Rewrite Bars to use new ProgressBar

--- src/components/Bars.js
+++ src/components/Bars.js
@@ -1,49 +1,75 @@
 import React from 'react';
-import { Progress } from 'reactstrap';
 import { CentreText } from './Text.js';
 import './SuccessBar.css';
-
-export const ValueBar = props => <Progress bar {...props}>{props.value}</Progress>
+import ProgressBar from './ProgressBar.js';
 
 export const SuccessBar = props => {
   let used = props.total - props.available;
   let remaining = props.target - props.total;
 
+  let usedBar = {
+    value: used,
+    text: used,
+    style: {
+      backgroundColor: "blue",
+      color: "white",
+    }
+  }
+
+  let availableBar = {
+    value: props.available,
+    text: props.available,
+    style: {
+      backgroundColor: "green",
+      color: "white",
+    }
+  }
+
+  let remainingBar = {
+    value: remaining,
+    text: remaining,
+    style: {
+      backgroundColor: "black",
+      color: "white",
+    }
+  }
+
   return (
     <div className={`${props.className || ''} SuccessBar`} key={props.key}>
       <CentreText>Successes</CentreText>
-      <Progress multi max={props.target}>
-        <ValueBar value={used} />
-        <ValueBar
-          animated
-          color="success"
-          className=""
-          value={props.available}
-        />
-        <ValueBar color="dark" value={remaining} />
-      </Progress>
-      <div>/{props.target}</div>
+      <ProgressBar max={props.target} bars={[usedBar, availableBar, remainingBar]}/>
+      <div>{props.total}/{props.target}</div>
     </div>
   );
 };
 
 export const MoteBar = props => {
+  let total = props.personal + props.peripheral
   let totalPool = props.personalPool + props.peripheralPool;
-  let total = props.personal + props.peripheral;
+
+  let personalBar = {
+    value: props.personal,
+    text: props.personal,
+    style: {
+      backgroundColor: "blue",
+      color: "white",
+    },
+  }
+
+  let peripheralBar = {
+    value: props.peripheral,
+    text: props.peripheral,
+    style: {
+      backgroundColor: "yellow",
+      color: "black",
+    },
+  }
+
   return (
-    <div className={`${props.className || ''} MoteBar`} key={props.key}>
-      <CentreText>Motes</CentreText>
-      <Progress color="success" value={total} max={totalPool}>
-        {total}
-      </Progress>
-      <Progress multi max={totalPool}>
-        <ValueBar value={props.personal} />
-        <ValueBar
-          value={props.peripheral}
-          color="warning"
-          className="text-dark"
-        />
-      </Progress>
+    <div className={`${props.className || ''} MoteBar`} key={props.key || "MoteBar"}>
+      <CentreText key="header" >Motes</CentreText>
+      <ProgressBar max={totalPool} bars={[personalBar, peripheralBar]}/>
+      {total}/{totalPool}
     </div>
   );
 };
