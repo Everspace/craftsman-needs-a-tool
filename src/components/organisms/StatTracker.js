65e7d2b1a9c8600f636fc36f68a084ce6ad82b61
unknown <7560635+Everspace@users.noreply.github.com>
2018-05-17 21:58:08 -0700

    Make the component jumble into organism

---
+++ src/components/organisms/StatTracker.js
@@ -0,0 +1,38 @@
+import React from "react"
+import Material from "components/atoms/Material"
+import { cssClass } from "styles/Colors"
+import MoteBar from "components/organisms/bars/MoteBar"
+import WillpowerBar from "components/organisms/bars/WillpowerBar"
+import SuccessBar from "components/organisms/bars/SuccessBar"
+
+export default props => {
+  return(
+    <Material
+      rounded spaced
+      className={cssClass.grey400}
+    >
+      <MoteBar
+        personal={props.personal}
+        personalPool={props.personalPool}
+        peripheral={props.peripheral}
+        peripheralPool={props.peripheralPool}
+      />
+
+      <WillpowerBar
+        willpower={props.willpower}
+        willpowerPool={props.willpowerPool}
+      />
+
+      <SuccessBar
+        total={props.successesTotal}
+        available={props.successesUsable}
+        target={props.successesTarget}
+      />
+      {props.children ?
+        <div>
+          {props.children}
+        </div>
+      : null}
+    </Material>
+  )
+}
