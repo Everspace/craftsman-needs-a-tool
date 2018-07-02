331c2fa8125a6f0c75bc091bfe76acd094f2f88a
unknown <7560635+Everspace@users.noreply.github.com>
2018-07-02 12:29:47 -0700

    document xony

--- src/lib/Style/xony.js
+++ src/lib/Style/xony.js
@@ -1,5 +1,10 @@
 import { css } from "emotion"
 
+/**
+ * Puts X color on Y background
+ * @param {Color} text
+ * @param {Color} background
+ */
 export const xony = (text, background) => css`
   color: ${text};
   background-color: ${background};
