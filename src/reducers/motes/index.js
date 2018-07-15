80aad7fac8c808b5b51459a56cf967bd82297bbc
unknown <7560635+Everspace@users.noreply.github.com>
2018-07-15 15:17:29 -0700

    Add more mote stuff to the mote reducer

--- src/reducers/motes/index.js
+++ src/reducers/motes/index.js
@@ -1,15 +1,30 @@
+import { MOTES_ADD, MOTES_DRIP, MOTES_FILL_POOLS } from "types/motes"
+import addMotesHandler from "./addMotesHandler"
+import { addMotes } from "actions/motes";
 
 const baseState = {
-  commitedMotes: [],
-
-  motes: {
-    personal: 0,
-    periferal: 0,
-  }
+  personal: 0,
+  periferal: 0,
+  personalPool: 0,
+  periferalPool: 0,
+  drip: 5,
 }
 
-export default function motes(state, action) {
+export default function motes(state = baseState, action) {
   switch (action.type) {
+    case MOTES_ADD:
+      return addMotesHandler(state, action)
+
+    case MOTES_DRIP:
+      return addMotesHandler(state, addMotes(0, state.drip))
+
+    case MOTES_FILL_POOLS:
+      return {
+        ...state,
+        personal: state.personalPool,
+        periferal: state.periferalPool,
+      }
+
     case undefined:
       return {...state, ...baseState}
 
