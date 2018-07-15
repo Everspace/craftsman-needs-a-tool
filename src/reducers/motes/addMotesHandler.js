5c21cb082144fbeb273d94790ac580b8c22493df
unknown <7560635+Everspace@users.noreply.github.com>
2018-07-15 15:15:34 -0700

    Do addMotesHandler

---
+++ src/reducers/motes/addMotesHandler.js
@@ -0,0 +1,29 @@
+export default (state, action) => {
+  let {personal, periferal} = action
+  let {personalPool, periferalPool} = state
+  let personalResult = state.personal
+  let periferalResult = state.periferal
+
+  personalResult += personal
+  periferalResult += periferal
+  // Handle added overflows
+  if (personalResult > personalPool) {
+    periferalResult += personalResult - personalPool
+  }
+  if (periferalResult > periferalPool) {
+    personalResult += periferalResult - periferalPool
+  }
+  // Cap them
+  if (personalResult > personalPool) {
+    personalResult = personalPool
+  }
+  if (periferalResult > periferalPool) {
+    periferalResult = periferalPool
+  }
+
+  return {
+    ...state,
+    personal: personalResult,
+    periferal: periferalResult,
+  }
+}
--- src/reducers/motes/addMotesHandler.test.js
+++ src/reducers/motes/addMotesHandler.test.js
@@ -12,64 +12,54 @@ describe("mote addition", () => {
   it('adds personal motes', () => {
     expect(
       reducer(initialMotes, addMotes(7))
-    ).toEqual(
-      {
-        personal: 17,
-        personalPool: 20,
-        periferal: 20,
-        periferalPool: 30,
-      }
-    )
+    ).toEqual({
+      personal: 17,
+      personalPool: 20,
+      periferal: 20,
+      periferalPool: 30,
+    })
   })
 
   it('adds periferal motes', () => {
     expect(
       reducer(initialMotes, addMotes(0, 7))
-    ).toEqual(
-      {
-        personal: 10,
-        personalPool: 20,
-        periferal: 27,
-        periferalPool: 30,
-      }
-    )
+    ).toEqual({
+      personal: 10,
+      personalPool: 20,
+      periferal: 27,
+      periferalPool: 30,
+    })
   })
 
   it('adds both at once', () => {
     expect(
       reducer(initialMotes, addMotes(7, 7))
-    ).toEqual(
-      {
-        personal: 17,
-        personalPool: 20,
-        periferal: 27,
-        periferalPool: 30,
-      }
-    )
+    ).toEqual({
+      personal: 17,
+      personalPool: 20,
+      periferal: 27,
+      periferalPool: 30,
+    })
   })
 
   it('adds to the other pool when overflowing', () => {
     expect(
       reducer(initialMotes, addMotes(11))
-    ).toEqual(
-      {
-        personal: 20,
-        personalPool: 20,
-        periferal: 21,
-        periferalPool: 30,
-      }
-    )
+    ).toEqual({
+      personal: 20,
+      personalPool: 20,
+      periferal: 21,
+      periferalPool: 30,
+    })
 
     expect(
       reducer(initialMotes, addMotes(0, 11))
-    ).toEqual(
-      {
-        personal: 11,
-        personalPool: 20,
-        periferal: 30,
-        periferalPool: 30,
-      }
-    )
+    ).toEqual({
+      personal: 11,
+      personalPool: 20,
+      periferal: 30,
+      periferalPool: 30,
+    })
   })
 
   it('wont go over maximums', () => {
