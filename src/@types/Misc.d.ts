e887ac43a4ad8decabad50acfb390b3291b5d70d
unknown <7560635+Everspace@users.noreply.github.com>
2018-10-28 19:00:27 -0700

    Write out rough shape for colors

--- src/@types/Misc.d.ts
+++ src/@types/Misc.d.ts
@@ -1,5 +1,5 @@
 declare namespace cnat {
-  export type Color = {
+  export type ColorPrimative = {
     r: number
     b: number
     g: number
@@ -12,3 +12,36 @@ type booleanString = "false" | "true"
 type IntrisnicElement = keyof JSX.IntrinsicElements
 type JSXElement<K extends IntrisnicElement> = JSX.IntrinsicElements[K]
 
+/**
+ * Actual color specification
+ */
+interface MaterialColorObject {
+  /**
+   * css classname from xony
+   */
+  cssClass: string
+  /**
+   * the color of the text
+   */
+  text: string
+  /**
+   * the background color that the object should be
+   */
+  color: string
+}
+
+/**
+ * A theme like "primary" or "secondary"
+ */
+interface MaterialColor {
+  light: MaterialColorObject
+  dark: MaterialColorObject
+  main: MaterialColorObject
+}
+
+/**
+ * Object accepts a Material color to be.
+ */
+interface Colorable {
+  color?: MaterialColor
+}
