18c194496e95dde1183057da75a5e1f77b6a80a2
unknown <7560635+Everspace@users.noreply.github.com>
2019-06-02 04:42:03 -0700

    Correct tests, migrate react-testing-library @testing-libary/react

--- src/components/organisms/bars/SuccessBar.test.tsx
+++ src/components/organisms/bars/SuccessBar.test.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import SuccessBar from "./SuccessBar"
 import { Provider } from "react-redux"
-import { render } from "react-testing-library"
+import { render } from "@testing-library/react"
 import { createStore } from "redux";
 
 const base = {
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,7 @@
 // this is basically: afterEach(cleanup)
-import "react-testing-library/cleanup-after-each"
+import "@testing-library/react/cleanup-after-each"
 
 // add some helpful assertions
 import "jest-dom/extend-expect"
 
-import "jest"
+export default undefined
