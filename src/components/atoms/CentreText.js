d192ac2c220a307a3e89200727fad4768c6df964
Everspace <7560635+Everspace@users.noreply.github.com>
2019-07-29 16:51:54 -0700

    Move to typescript

--- src/components/atoms/CentreText.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { cx } from "emotion"
-
-const CenterText = props => (
-  <div {...props} className={cx(`text-center`, props.className)} />
-)
-
-export default CenterText
---
+++ src/components/atoms/CentreText.tsx
@@ -0,0 +1,8 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core"
+
+const CenterText: React.FC<{}> = props => (
+  <div {...props} className={`text-center`} />
+)
+
+export default CenterText
